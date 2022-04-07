8415ebdabf82ce04cea5491ab696ed3e0e440a01
shibafu <shibafu528@gmail.com>
2022-04-08 00:17:00 +0900

    bin/yqv1: -v argument support

--- src/bin/yqv1.rs
+++ src/bin/yqv1.rs
@@ -1,25 +1,40 @@
+use clap::Parser;
 use std::collections::HashMap;
-use std::{env, process};
+use std::process;
 use yq::v1::eval::{Context, VariableProvider};
 use yq::v1::expr::{Atom, Expression};
 
+#[derive(Parser, Debug)]
+#[clap(author, version)]
+struct Args {
+    /// Input query string
+    query: String,
+
+    /// Provide variable (format must be 'key=value')
+    #[clap(short, long)]
+    value: Vec<String>,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("invalid argument");
-        process::exit(1);
+    let args = Args::parse();
+    let mut variables = HashMap::new();
+    for v in &args.value {
+        match v.split_once('=') {
+            Some((k, v)) => {
+                variables.insert(k.to_string(), Expression::Atom(Atom::String(v.to_string())))
+            }
+            _ => {
+                eprintln!("--value format must be 'key=value'");
+                process::exit(1);
+            }
+        };
     }
 
-    let query = yq::v1::parser::parse(&args[1]).unwrap();
+    let query = yq::v1::parser::parse(&args.query).unwrap();
     println!("==> Parse result:\n{:#?}\n", query);
 
     let mut context = Context::new();
-    let provider = Box::new(Provider {
-        values: HashMap::from([(
-            "?source",
-            Expression::Atom(Atom::String("Yukari for Android".to_string())),
-        )]),
-    });
+    let provider = Box::new(Provider { values: variables });
     context.set_variable_provider(provider);
 
     let result = context.evaluate(query.expression());
@@ -27,7 +42,7 @@ fn main() {
 }
 
 struct Provider {
-    values: HashMap<&'static str, Expression>,
+    values: HashMap<String, Expression>,
 }
 
 impl VariableProvider for Provider {
